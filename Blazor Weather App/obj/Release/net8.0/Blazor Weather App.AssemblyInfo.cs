2ff5c5eb259d6e839579b8846696b43b9c001972
Keith Chidamba <kidakwashe14@gmail.com>
2025-03-05 11:37:27 -0800

    build new

--- Blazor Weather App/obj/Release/net8.0/Blazor Weather App.AssemblyInfo.cs
+++ Blazor Weather App/obj/Release/net8.0/Blazor Weather App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor Weather App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3920261a3ff4f110eecf51d9d748d04a148d32cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826e602dca812e851d19cdae3862d90762ebdfb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor Weather App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor Weather App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
