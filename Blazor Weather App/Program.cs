3920261a3ff4f110eecf51d9d748d04a148d32cc
Keith Chidamba <kidakwashe14@gmail.com>
2025-03-05 10:59:38 -0800

    link 4

--- Blazor Weather App/Program.cs
+++ Blazor Weather App/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://keithchidamba.github.io/Blazor-Weather-App/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://nominatim.openstreetmap.org/") });
 
 await builder.Build().RunAsync();
